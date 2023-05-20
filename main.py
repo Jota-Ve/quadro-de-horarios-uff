8656ca0d0a9ba90be19006f4404a6e79936c0518
João Vitor Barbosa <j.vs.b13@gmail.com>
2023-05-20 14:50:18 -0300

    Adiciona pesquisa básica

--- main.py
+++ main.py
@@ -2,25 +2,63 @@ from pathlib import Path
 from time import sleep
 import bs4
 import requests
+import logging
 
-LINK_QUADRO_HORARIOS = r'https://app.uff.br/graduacao/quadrodehorarios/'
+logging.basicConfig(level=logging.DEBUG)
+
+PAGINA_INICIAL = r'https://app.uff.br/graduacao/quadrodehorarios/'
+home_sufixo = r'?utf8=%E2%9C%93&q%5B'
+valor_prefixo = r'%5D='
+separador = r'&q%5B'
+
+# Pesquisando 'TESTE' com semestre 2023.1
+# 'https://app.uff.br/graduacao/quadrodehorarios/?utf8=%E2%9C%93&q%5Bdisciplina_nome_or_disciplina_codigo_cont%5D=TESTE&q%5Banosemestre_eq%5D=20231&q%5Bdisciplina_cod_departamento_eq%5D=&button=&q%5Bidturno_eq%5D=&q%5Bidlocalidade_eq%5D=&q%5Bvagas_turma_curso_idcurso_eq%5D=&q%5Bcurso_ferias_eq%5D=&q%5Bidturmamodalidade_eq%5D='
+    # Logado
+    # 'https://app.uff.br/graduacao/quadrodehorarios/?utf8=%E2%9C%93&q%5Bdisciplina_nome_or_disciplina_codigo_cont%5D=TESTE&q%5Banosemestre_eq%5D=20231&q%5Bdisciplina_cod_departamento_eq%5D=&button=&q%5Bidturno_eq%5D=&q%5Bpor_professor%5D=&q%5Bidlocalidade_eq%5D=&q%5Bvagas_turma_curso_idcurso_eq%5D=&q%5Bcurso_ferias_eq%5D=&q%5Bidturmamodalidade_eq%5D='
+# Pesquisando 'AAA' com semestre 2023.1
+# 'https://app.uff.br/graduacao/quadrodehorarios/?utf8=%E2%9C%93&q%5Bdisciplina_nome_or_disciplina_codigo_cont%5D=AAA&q%5Banosemestre_eq%5D=20231&q%5Bdisciplina_cod_departamento_eq%5D=&button=&q%5Bidturno_eq%5D=&q%5Bidlocalidade_eq%5D=&q%5Bvagas_turma_curso_idcurso_eq%5D=&q%5Bcurso_ferias_eq%5D=&q%5Bidturmamodalidade_eq%5D='
+    
 
 
-def request_link(link: str) -> requests.Response:
-    sleep(2) # Atraso no request para não sobrecarregar a página
-    response = requests.get(link)
-    return response
+def pesquisa_link(disciplina_nome_ou_codigo: str|None = None, 
+                  ano_semestre: str|None = None, 
+                  departamento: str|None = None, 
+                  turno: str|None = None,
+                  professor: str|None = None, 
+                  localidade: str|None = None, 
+                  vagas_curso: str|None = None, 
+                  curso_ferias: str|None = None, 
+                  turma_modalidade: str|None = None) -> requests.Response:
+    
+    parametros = {
+        'utf8': '%E2%9C%93',
+        'q[disciplina_nome_or_disciplina_codigo_cont]': disciplina_nome_ou_codigo, 
+        'q[anosemestre_eq]': ano_semestre, 
+        'q[disciplina_cod_departamento_eq]': departamento, 
+        'q[idturno_eq]': turno,
+        'q[por_professor_eq]': professor, 
+        'q[idlocalidade_eq]': localidade, 
+        'q[vagas_turma_curso_idcurso_eq]': vagas_curso, 
+        'q[curso_ferias_eq]': curso_ferias, 
+        'q[idturmamodalidade_eq]': turma_modalidade
+    }
+    
+    logging.debug("Esperando para pesquisar")
+    sleep(1.5) # Atraso no request para não sobrecarregar a página
+    logging.info(f"Pesquisando")
+    resposta = requests.get(PAGINA_INICIAL, params=parametros)
+    return resposta
 
 
-def save_source_code(response: requests.Response, path: str|Path):
-    source_code = response.text
-    soup = bs4.BeautifulSoup(source_code, features='lxml')
+def salva_HTML(resposta: requests.Response, path: str|Path):
+    logging.debug(f"Salvando HTML: {Path(path).name}")
+    soup = bs4.BeautifulSoup(resposta.text, features='lxml')
     Path(path).write_text(soup.prettify(), encoding='utf-8')
     
 
 def main():
-    response = request_link(LINK_QUADRO_HORARIOS)
-    save_source_code(response, 'quadro_horarios.html')
+    resposta = pesquisa_link()
+    salva_HTML(resposta, 'test_busca.html')
 
 
 if __name__ == '__main__':
