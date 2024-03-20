307bb7cd00b4e240b8c0ab0add575bd0324cc505
João Vitor Barbosa <j.vs.b13@gmail.com>
2024-03-20 12:07:44 -0300

    Adiciona filtro de vagas para curso

--- lista_disciplinas.py
+++ lista_disciplinas.py
@@ -1,7 +1,8 @@
 import logging
 from pathlib import Path
-from time import sleep
+import time
 from typing import Literal
+import re
 
 import bs4
 import requests
@@ -17,11 +18,26 @@ class ListaDisciplinas:
                 #  turno: str|None = None, 
                 #  professor: str|None = None,  
                 #  localidade: str|None = None, 
-                #  vagas_curso: str|None = None, 
+                 vagas_curso: str|None = None, 
                 #  curso_ferias: str|None = None, 
                 #  turma_modalidade: str|None = None
                  ):
         
+        cod_curso = ""
+        
+        if vagas_curso:
+            soup = bs4.BeautifulSoup(requests.get(self.PAGINA_INICIAL).text, features='lxml')
+            time.sleep(.3)
+            lista_vagas_curso = soup.find(id="q_vagas_turma_curso_idcurso_eq")
+            curso = lista_vagas_curso.find(text=re.compile(f'^{vagas_curso}$', re.RegexFlag.IGNORECASE)) #type: ignore
+            
+            # Se achou o curso, pega o código. Se não, usa código inválido 
+            # para não gerar resultados falsos positivos
+            if curso is not None:
+                cod_curso = curso.parent['value'] #type: ignore
+            else:
+                cod_curso = "-1"
+                    
         parametros = {
             'utf8': '✓',
             'q[disciplina_nome_or_disciplina_codigo_cont]': nome_ou_codigo, 
@@ -30,13 +46,10 @@ class ListaDisciplinas:
             # 'q[idturno_eq]': turno,
             # 'q[por_professor_eq]': professor, 
             # 'q[idlocalidade_eq]': localidade, 
-            # 'q[vagas_turma_curso_idcurso_eq]': vagas_curso, 
+            'q[vagas_turma_curso_idcurso_eq]': cod_curso, 
             # 'q[curso_ferias_eq]': curso_ferias, 
             # 'q[idturmamodalidade_eq]': turma_modalidade
         }
-        
-        logging.debug("Esperando para pesquisar")
-        sleep(0.5) # Atraso no request para não sobrecarregar a página
         
         logging.info(f"Pesquisando")
         self.__resposta_do_request = requests.get(self.PAGINA_INICIAL, params=parametros)
