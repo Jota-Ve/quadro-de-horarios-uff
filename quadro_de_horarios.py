2b7ad8dd960c93f8f24bf5eaf723be0ea3249c5a
João Vitor Barbosa <j.vs.b13@gmail.com>
2024-03-24 23:17:28 -0300

    Melhorias na documentação

--- quadro_de_horarios.py
+++ quadro_de_horarios.py
@@ -10,15 +10,14 @@ from lista_disciplinas import ListaDisciplinas
 
 
 class QuadroDeHorarios():
+    """Classe para a busca de disciplinas utilizando os filtros disponíveis"""
+    
     PAGINA_INICIAL = r'https://app.uff.br/graduacao/quadrodehorarios/'
 
     def __init__(self):
         self._soup = bs4.BeautifulSoup(requests.get(self.PAGINA_INICIAL).text, features='lxml')
         
         self._parametros = {
-            # 'utf8': '✓',
-            # 'q[disciplina_nome_or_disciplina_codigo_cont]': nome_ou_codigo, 
-            # 'q[anosemestre_eq]': f'{ano_semestre[0]}{ano_semestre[1]}', 
             # 'q[disciplina_cod_departamento_eq]': departamento, 
             # 'q[idturno_eq]': turno,
             # 'q[por_professor_eq]': professor, 
@@ -29,10 +28,12 @@ class QuadroDeHorarios():
     
     
     def seleciona_ano_semestre(self, ano: int, semestre: Literal[1, 2]):
+        """Filtro de ano e semestre das turmas"""
         self._parametros['q[anosemestre_eq]'] = f'{ano}{semestre}' 
     
     
     def seleciona_vagas_para_curso(self, curso: str):
+        """Filtro de turmas com vagas para o curso informado"""
         lista_vagas_curso = self._soup.find(id="q_vagas_turma_curso_idcurso_eq")
         tag_curso = lista_vagas_curso.find(text=re.compile(f'^{curso}$', re.RegexFlag.IGNORECASE)) #type: ignore
     
@@ -43,6 +44,9 @@ class QuadroDeHorarios():
     
     
     def pesquisa(self, cod_ou_nome_dicsciplina: str=""):
+        """Pesquisa código ou nome da turma informado, levando 
+        em conta os possíveis filtros confiugurados anteriormente"""
+        
         self._parametros['utf8'] = '✓'
         self._parametros['q[disciplina_nome_or_disciplina_codigo_cont]'] = cod_ou_nome_dicsciplina
 
@@ -52,5 +56,6 @@ class QuadroDeHorarios():
         
     
     def limpa_filtros(self):
+        """Remove todos os filtros configurados"""
         self._parametros.clear()
         
