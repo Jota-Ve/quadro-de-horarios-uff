f42b63252efa5e1845467692af07b3aaaabb3b4f
João Vitor Barbosa <j.vs.b13@gmail.com>
2024-03-25 02:37:29 -0300

    Classe Disciplina

--- lista_disciplinas.py
+++ lista_disciplinas.py
@@ -1,7 +1,39 @@
 
+import enum
+import re
+from typing import Iterable
+
 import bs4
 
 
+class DiaDaSemana(enum.StrEnum):
+    SEGUNDA = "horario-segunda"
+    TERCA   = "horario-terca"
+    QUARTA  = "horario-quarta"
+    QUINTA  = "horario-quinta"
+    SEXTA   = "horario-sexta"
+    SABADO  = "horario-sabado"
+
+
+
+
+class Disciplina:
+    def __init__(self, soup: bs4.BeautifulSoup) -> None:
+        self._soup = soup
+        tags = self._soup.find_all('td')
+        self.ano_semestre = self._soup['data-anosemestre'].strip()
+        self.codigo = tags[0].get_text().strip()
+        self.nome = tags[1].get_text().strip()
+        self.turma = tags[2].get_text().strip()
+        self.modulo = tags[3].get_text().strip()
+        self.tipo_de_oferta = tags[4].get_text().strip()
+        
+        RGXP_HORARIO = re.compile(r'^\d\d:\d\d-\d\d:\d\d')
+        FILTRO_DIAS_COM_HORARIO = {'text': RGXP_HORARIO, 'attrs': {'class': list(DiaDaSemana)}}
+        semana = self._soup.find_all(**FILTRO_DIAS_COM_HORARIO)
+        self.horario = {DiaDaSemana(tag_dia['class'][0]): tag_dia.get_text().split(',') for tag_dia in semana}
+          
+        
 class ListaDisciplinas:
     
     def __init__(self, soup: bs4.BeautifulSoup):
@@ -13,3 +45,38 @@ class ListaDisciplinas:
             return []
         
         return [disc.text.strip() for disc in self._soup.find_all(attrs={'class':'disciplina-nome'})]
+
+    
+    def horarios(self, dia_horario: dict[DiaDaSemana, list[str]]):
+        # def em_intervalo(horario: tuple[str, str], intervalos: Iterable[str]):
+        #     for dia_tag in dias:
+        #         dia = DiaDaSemana(dia_tag['class'][0])
+        #         for inicio, fim in map(lambda x: x.split('-'), dia_horario[dia]):
+                    
+        #     return any(inicio <= horario[0] <= horario[1] <= fim 
+        #                for inicio, fim in map(lambda x: x.split('-'), intervalos))
+            
+        RGXP_HORARIO = re.compile(r'^\d\d:\d\d-\d\d:\d\d')
+        # for dia, intervalos in dia_horario.items():
+            # inicio_intervalo, final_intervalo = intervalo.split('-')
+        for tag in self._soup.find_all(text=RGXP_HORARIO, attrs={'class': list(dia_horario)}):
+            disc = Disciplina(tag.parent)
+            print(f'{disc.codigo} - {disc.turma} -> {disc.nome}\n\t{disc.horario}\n')
+            
+        # dia = DiaDaSemana(tag['class'][0])
+        #     horario1, *horario2 = tag.get_text().split(',')
+            
+        #     if not em_intervalo(horario1.split('-'), dia_horario[dia]):
+        #         continue
+            
+        #     if horario2 and not em_intervalo(horario2[0], dia_horario[dia]):
+        #         continue
+            
+        #     print(tag)
+        #     print('')
+        #     FILTRO_DIAS_COM_HORARIO = {'text': RGXP_HORARIO, 'attrs': {'class': list(DiaDaSemana)}}
+        #     semana = (tag 
+        #               + tag.find_previous_siblings(**FILTRO_DIAS_COM_HORARIO)
+        #               + tag.find_next_siblings(**FILTRO_DIAS_COM_HORARIO))
+            
+            
--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import logging
 
 import quadro_de_horarios
+from lista_disciplinas import DiaDaSemana
 
 logging.basicConfig(level=logging.DEBUG)
 
-home_sufixo = r'?utf8=%E2%9C%93&q%5B'
-valor_prefixo = r'%5D='
-separador = r'&q%5B'
-
-# Pesquisando 'TESTE' com semestre 2023.1
-# 'https://app.uff.br/graduacao/quadrodehorarios/?utf8=%E2%9C%93&q%5Bdisciplina_nome_or_disciplina_codigo_cont%5D=TESTE&q%5Banosemestre_eq%5D=20231&q%5Bdisciplina_cod_departamento_eq%5D=&button=&q%5Bidturno_eq%5D=&q%5Bidlocalidade_eq%5D=&q%5Bvagas_turma_curso_idcurso_eq%5D=&q%5Bcurso_ferias_eq%5D=&q%5Bidturmamodalidade_eq%5D='
-    # Logado
-    # 'https://app.uff.br/graduacao/quadrodehorarios/?utf8=%E2%9C%93&q%5Bdisciplina_nome_or_disciplina_codigo_cont%5D=TESTE&q%5Banosemestre_eq%5D=20231&q%5Bdisciplina_cod_departamento_eq%5D=&button=&q%5Bidturno_eq%5D=&q%5Bpor_professor%5D=&q%5Bidlocalidade_eq%5D=&q%5Bvagas_turma_curso_idcurso_eq%5D=&q%5Bcurso_ferias_eq%5D=&q%5Bidturmamodalidade_eq%5D='
-# Pesquisando 'AAA' com semestre 2023.1
-# 'https://app.uff.br/graduacao/quadrodehorarios/?utf8=%E2%9C%93&q%5Bdisciplina_nome_or_disciplina_codigo_cont%5D=AAA&q%5Banosemestre_eq%5D=20231&q%5Bdisciplina_cod_departamento_eq%5D=&button=&q%5Bidturno_eq%5D=&q%5Bidlocalidade_eq%5D=&q%5Bvagas_turma_curso_idcurso_eq%5D=&q%5Bcurso_ferias_eq%5D=&q%5Bidturmamodalidade_eq%5D='
-    
 
 def main():
     quadro = quadro_de_horarios.QuadroDeHorarios()
     quadro.seleciona_ano_semestre(2024, 1)
     quadro.seleciona_vagas_para_curso("Sistemas de informação")
     lista_disc = quadro.pesquisa()
-    print(f"Disciplinas: {lista_disc.nome_disciplinas()}")
+    print(f"\nDisciplinas: {lista_disc.nome_disciplinas()}\n")
+    
+    print(lista_disc.horarios({
+        DiaDaSemana.SEGUNDA: ["00:00-23:59"],
+        DiaDaSemana.TERCA: ["12:00-23:59"]}
+    ))
     print("")
     # lista_disc.salvar_HTML("test_busca.html")
 
