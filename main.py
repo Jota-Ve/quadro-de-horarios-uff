b471dca70f900a5bbafe9bf66924629dae3bff83
João Vitor Barbosa <joao.barbosa@regulacaoriorj.com.br>
2024-06-05 19:23:46 -0300

    Função para salvar VSC de disciplinas e horários

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+from datetime import datetime
 import logging
+from typing import Iterable
 
 import quadro_de_horarios
-from lista_disciplinas import DiaDaSemana
+from lista_disciplinas import DiaDaSemana, ListaDisciplinas
 
 logging.basicConfig(level=logging.INFO)
+
+
+def salva_disciplinas_e_horarios(lista_disc: Iterable[ListaDisciplinas]):
+    for lista in lista_disc:
+        for disciplina in lista.disciplinas:
+            with (open(f'disciplinas-{datetime.now().isoformat()}.csv', 'w', encoding='utf-8') as f_disc,
+                  open(f'horarios-{datetime.now().isoformat()}.csv', 'w', encoding='utf-8') as f_hora):
+
+                # Disciplinas
+                f_disc.write(';'.join([
+                    disciplina.ano_semestre, disciplina.tipo_de_oferta,
+                    disciplina.codigo, disciplina.nome, disciplina.modulo,
+                    disciplina.turma
+                ]) + '\n')
+
+                # Horários
+                for dia, horarios in disciplina.horario.items():
+                    for hora in horarios:
+                        f_hora.write(';'.join([
+                            disciplina.ano_semestre, disciplina.codigo,
+                            disciplina.turma, dia.name, hora[:5], hora[6:]
+                        ]) + '\n')
 
 
 def main():
@@ -12,16 +36,9 @@ def main():
     quadro.seleciona_vagas_para_curso("Sistemas de informação")
     lista_disc = quadro.pesquisa()
     # print(f"\nDisciplinas: {lista_disc.nome_disciplinas()}\n")
-    
-    disciplinas = lista_disc.selecionar_horarios({
-        DiaDaSemana.SEGUNDA: ["00:00-22:00"],
-        DiaDaSemana.TERCA: ["18:00-23:59"],
-        DiaDaSemana.QUARTA: ["20:00-23:59"],
-        
-        }
-    )
 
-    print('\n'.join(map(str, disciplinas)))
+    salva_disciplinas_e_horarios(lista_disc)
+
     print("")
     # lista_disc.salvar_HTML("test_busca.html")
 
