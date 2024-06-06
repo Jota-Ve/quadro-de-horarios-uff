c2bdf2b32cfdf5b77d37b5aa3dc322479ce3f344
João Vitor Barbosa <j.vs.b13@gmail.com>
2024-06-06 00:34:56 -0300

    Utilizando o cli

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 
+import horario
 import quadro_de_horarios
 from lista_disciplinas import DiaDaSemana
 import cli
@@ -9,21 +10,16 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     args = cli.pega_argumentos()
+    logging.debug(f"Argumentos: {args}")
+    
     quadro = quadro_de_horarios.QuadroDeHorarios()
     quadro.seleciona_ano_semestre(*args.ano_semestre)
-    quadro.seleciona_vagas_para_curso(args.curso)
+    # quadro.seleciona_vagas_para_curso(args.curso)
     lista_disc = quadro.pesquisa(args.pesquisa)
-    print(f"\nDisciplinas: {lista_disc.nome_disciplinas()}\n")
     
-    disciplinas = lista_disc.selecionar_horarios({
-        DiaDaSemana.SEGUNDA: ["00:00-22:00"],
-        DiaDaSemana.TERCA: ["10:00-23:59"],
-        DiaDaSemana.QUARTA: ["10:00-23:59"],
-        
-        }
-    )
+    disciplinas = lista_disc.selecionar_horarios(args.horarios)
 
-    print(f"Filtrando horários: {'\n'.join(map(str, disciplinas))}")
+    print(f"Filtrando horários: {'\n'.join(map(str, lista_disc.disciplinas))}")
     print("")
     # lista_disc.salvar_HTML("test_busca.html")
 
