36ace4039a30d8cea06b5fada67b6ddb6d0356c6
João Vitor Barbosa <j.vs.b13@gmail.com>
2024-06-21 14:24:04 -0300

    Prototipop abrindo dados de reprovados

--- relatorio.py
+++ relatorio.py
@@ -1,3 +1,4 @@
+import re
 from typing import Literal
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -9,7 +10,8 @@ class Relatorios:
     PAGINA_INICIAL = r'https://app.uff.br/graduacao/quadrodehorarios/relatorios'
     
     _LOC_DEPARTAMENTO = (By.ID, 'iddepartamento_coordenacao')
-    _LOC_REPROVADOS_SEMESTRE   = (By.XPATH, r'//h4[text()="Relatório reprovados por turma"]/parent::*/parent::*//select[@name="anosemestre"]')
+    _LOC_SEMESTRE_REPROVADOS = (By.XPATH, r'//h4[text()="Relatório reprovados por turma"]/parent::*/parent::*//select[@name="anosemestre"]')
+    _LOC_DADOS_REPROVADOS = (By.XPATH, r'//button[@aria-label="Visualizar dados do relatório de reprovados por coordenação"]')
     
     
     def __init__(self) -> None:
@@ -34,15 +36,47 @@ class Relatorios:
     
     
     def semestre_atual(self) -> str | None:
-        select_element = self._driver.find_element(*self._LOC_REPROVADOS_SEMESTRE)
+        select_element = self._driver.find_element(*self._LOC_SEMESTRE_REPROVADOS)
         return Select(select_element).first_selected_option.get_attribute('value')
     
-    def seleciona_semestre(self, ano: int, semestre: Literal[1, 2]) -> str | None:
-        select = Select(self._driver.find_element(*self._LOC_REPROVADOS_SEMESTRE))
-        for i, elemento in enumerate(select.options):
+    def abre_reprovados(self, ano: int, semestre: Literal[1, 2]) -> str | None:
+        # Seleciona o semestre
+        select_semestre = Select(self._driver.find_element(*self._LOC_SEMESTRE_REPROVADOS))
+        for elemento in select_semestre.options:
             if (valor := elemento.get_attribute('value')) == f'{ano}{semestre}':
-                select.select_by_value(valor) #type: ignore
-                return valor
+                select_semestre.select_by_value(valor) #type: ignore
+                break
+        
+        else: # Não encontrou (n ativou o break)
+            return None      
+            
+        # Abre a página de reprovados
+        n_janelas = len(self._driver.window_handles)
+        handle_original = self._driver.current_window_handle
+        
+        wait = WebDriverWait(self._driver, 10)
+        self._driver.find_element(*self._LOC_DADOS_REPROVADOS).click()
+        wait.until(EC.number_of_windows_to_be(n_janelas+1))
+
+        # Loop through until we find a new window handle
+        for window_handle in self._driver.window_handles:
+            if window_handle == handle_original:
+                continue
+            
+            self._driver.switch_to.window(window_handle)
+            if 'reprovados_coordenacao' in self._driver.current_url:
+                break
+        else:
+            raise RuntimeError("Não achou janlea de reprovados_coordenacao")
+        
+        _LOC_NUM_RESULTADOS = (By.XPATH, '//select[@name="tabela-reprovados_length"]')
+        select_num_resultados = Select(self._driver.find_element(*_LOC_NUM_RESULTADOS))
+        max_num = max([int(op.get_attribute('value')) for op in select_num_resultados.options])
+        select_num_resultados.select_by_value(str(max_num))
+        
+        resultados = [re.match(r'(\w+\d+) (.+) (\d+)', el.text) for el in self._driver.find_elements(By.TAG_NAME, 'tr')][1:]
+        dados = sorted([(x.group(1), x.group(2), int(x.group(3))) for x in resultados], key=lambda x: x[2])
+        
         
     
     
