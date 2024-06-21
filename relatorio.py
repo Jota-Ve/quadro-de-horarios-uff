87a84bdec07409ab9eec499da938fa3f73e7360f
João Vitor Barbosa <j.vs.b13@gmail.com>
2024-06-21 12:04:17 -0300

    Seleciona semestre dos reprovados

--- relatorio.py
+++ relatorio.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +9,8 @@ class Relatorios:
     PAGINA_INICIAL = r'https://app.uff.br/graduacao/quadrodehorarios/relatorios'
     
     _LOC_DEPARTAMENTO = (By.ID, 'iddepartamento_coordenacao')
+    _LOC_REPROVADOS_SEMESTRE   = (By.XPATH, r'//h4[text()="Relatório reprovados por turma"]/parent::*/parent::*//select[@name="anosemestre"]')
+    
     
     def __init__(self) -> None:
         self._driver = webdriver.Chrome()
@@ -28,7 +31,18 @@ class Relatorios:
                 dep_selecionado = elemento.text
                 select.select_by_index(i)
                 return dep_selecionado
-        
+    
+    
+    def semestre_atual(self) -> str | None:
+        select_element = self._driver.find_element(*self._LOC_REPROVADOS_SEMESTRE)
+        return Select(select_element).first_selected_option.get_attribute('value')
+    
+    def seleciona_semestre(self, ano: int, semestre: Literal[1, 2]) -> str | None:
+        select = Select(self._driver.find_element(*self._LOC_REPROVADOS_SEMESTRE))
+        for i, elemento in enumerate(select.options):
+            if (valor := elemento.get_attribute('value')) == f'{ano}{semestre}':
+                select.select_by_value(valor) #type: ignore
+                return valor
         
     
     
