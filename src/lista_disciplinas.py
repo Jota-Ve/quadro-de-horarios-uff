4fef1d2a61c5ece250f7752e33ecd13ce19aedd9
João Vitor Barbosa <j.vs.b13@gmail.com>
2025-09-14 00:53:12 -0300

    Renomeia atributos e métodos

--- src/lista_disciplinas.py
+++ src/lista_disciplinas.py
@@ -17,8 +17,7 @@ import requisicao
 
 logger = logging.getLogger(__name__)
 
-#TODO: Mudar o nome das classes pois elas representam Turmas individuais (TCC0030 - A1 em 2025.1), e não Disciplinas (TCC0030)
-class DisciplinaInfo:
+class TurmaInfo:
     #TODO: Suportar DisciplinaInfo vazia, sem informações, e ser == False nesse caso
     RGX_TITULO = re.compile(r'Turma ([\w\d]+) de ([\w\d]+) - (.*)', re.IGNORECASE)
     RGX_SEMESTRE = re.compile(r'\d+')
@@ -28,7 +27,7 @@ class DisciplinaInfo:
         self._url: str = r'https://app.uff.br' + soup.find('form', attrs={'class': re.compile("^edit_turma")})['action']
         logger.debug(self._url)
 
-        self._id_turma    : int = int(self._url.rsplit('/', 1)[1])
+        self._id    : int = int(self._url.rsplit('/', 1)[1])
         match = self.RGX_TITULO.search(soup.h1.text.strip())
         self.turma        : str = match.group(1)
         self.codigo       : str = match.group(2)
@@ -70,8 +69,8 @@ class DisciplinaInfo:
 
 
     @property
-    def id_turma(self) -> int:
-        return self._id_turma
+    def id(self) -> int:
+        return self._id
 
     @property
     def url(self) -> str:
@@ -79,91 +78,115 @@ class DisciplinaInfo:
 
 
 
-class Disciplina:
+class Turma:
+    """Representa uma turma de disciplina da UFF extraída de uma linha de tabela HTML.
+    Permite acessar informações básicas e detalhadas da turma, como horários e dados via requisição HTTP.
+    """
+
     _SESSION = requests.Session()
 
     def __init__(self, soup: bs4.BeautifulSoup) -> None:
         self._soup = soup
         tags = self._soup.find_all('td')
-        self._url_info      : str = r'https://app.uff.br' + tags[0].contents[0]['href']
-        self._id            : int = int(self._url_info.rsplit('/', 1)[1])
-        self.ano_semestre   : str = self._soup['data-anosemestre'].strip()
-        self.codigo         : str = tags[0].get_text().strip()
-        self.nome           : str = tags[1].get_text().strip()
-        self.turma          : str = tags[2].get_text().strip()
-        self.modulo         : str = tags[3].get_text().strip()
-        self.tipo_de_oferta : str = tags[4].get_text().strip()
+        self._url_info          : str = r'https://app.uff.br' + tags[0].contents[0]['href']
+        self._id                : int = int(self._url_info.rsplit('/', 1)[1])
+        self._ano_semestre      : str = self._soup['data-anosemestre'].strip()
+        self._codigo_disciplina : str = tags[0].get_text().strip()
+        self._nome_disciplina   : str = tags[1].get_text().strip()
+        self._nome              : str = tags[2].get_text().strip()
+        self._modulo            : str = tags[3].get_text().strip()
+        self._tipo_de_oferta    : str = tags[4].get_text().strip()
 
-        self.horario: dict[horario.DiaDaSemana, list[horario.Horario]] = {}
+        self._horario: dict[horario.DiaDaSemana, list[horario.Horario]] = {}
         RGXP_HORARIO = re.compile(r'^\d\d:\d\d-\d\d:\d\d')
         FILTRO_DIAS_COM_HORARIO = {'text': RGXP_HORARIO, 'attrs': {'class': list(horario.DiaDaSemana)}}
 
         for tag_dia in self._soup.find_all(**FILTRO_DIAS_COM_HORARIO):
-            self.horario[horario.DiaDaSemana(tag_dia['class'][0])] = [horario.Horario(h) for h in tag_dia.get_text().split(',')]
+            self._horario[horario.DiaDaSemana(tag_dia['class'][0])] = [horario.Horario(h) for h in tag_dia.get_text().split(',')]
 
 
+    #region Getters
+    @property
+    def id(self) -> int: return self._id
+    @property
+    def url_info(self) -> str: return self._url_info
+    @property
+    def ano_semestre(self) -> str: return self._ano_semestre
+    @property
+    def codigo_disciplina(self) -> str: return self._codigo_disciplina
+    @property
+    def nome_disciplina(self) -> str: return self._nome_disciplina
+    @property
+    def nome(self) -> str: return self._nome
+    @property
+    def modulo(self) -> str: return self._modulo
+    @property
+    def tipo_de_oferta(self) -> str: return self._tipo_de_oferta
+    @property
+    def horario(self) -> dict[horario.DiaDaSemana, list[horario.Horario]]: return self._horario
+    #endregion
+
+    #region Dunder methods
     def __str__(self) -> str:
-        return (f'{self.codigo} - {self.nome} ({self.turma}): '
-                + ', '.join([f'{dia.name[:3]}={hora}' for dia, hora in self.horario.items()]))
+        return (f'{self._codigo_disciplina} - {self._nome_disciplina} ({self._nome}): '
+                + ', '.join([f'{dia.name[:3]}={hora}' for dia, hora in self._horario.items()]))
 
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}({self.__str__()})'
+    #endregion
 
 
-    @property
-    def id(self) -> int:
-        return self._id
-
-    @property
-    def url_info(self) -> str:
-        return self._url_info
-
-
-    async def async_info(self, session: aiohttp.ClientSession, limite: asyncio.Semaphore, espera_aleatoria: tuple[float, float]|None=(.05, .75)) -> DisciplinaInfo|None:
+    async def async_info(self, session: aiohttp.ClientSession, limite: asyncio.Semaphore, espera_aleatoria: tuple[float, float]|None=(.05, .75)) -> TurmaInfo|None:
         #TODO: Retornar apenas DisciplinaInfo, mas caso seja vazio ela ser == False
         soup = await requisicao.async_request(session, limite, self._url_info, espera_aleatoria=espera_aleatoria)
         if isinstance(info_soup := soup.find('div', attrs={'class': "container-fluid mt-3"}), bs4.Tag):
-            return DisciplinaInfo(info_soup)
+            return TurmaInfo(info_soup)
 
 
-    def info(self) -> DisciplinaInfo|None:
+    def info(self) -> TurmaInfo|None:
         #TODO: Retornar apenas DisciplinaInfo, mas caso seja vazio ela ser == False
         soup = bs4.BeautifulSoup(self._SESSION.get(self._url_info).text, features='lxml')
         info_soup: bs4.Tag|None = soup.find('div', attrs={'class': "container-fluid mt-3"})
         if info_soup is not None:
-            return DisciplinaInfo(info_soup)
+            return TurmaInfo(info_soup)
 
 
 
-class ListaDisciplinas:
+class ListaTurmas:
 
     def __init__(self, soup: bs4.BeautifulSoup):
         self._soup: bs4.Tag|None = soup if soup.get('id') == "tabela-turmas" else soup.find(id="tabela-turmas")
         if self._soup is not None:
-            self.disciplinas = [Disciplina(tag) for tag in self._soup.tbody.find_all('tr')]
+            self._turmas = [Turma(tag) for tag in self._soup.tbody.find_all('tr')]
         else:
-            self.disciplinas = []
+            self._turmas = []
 
 
-    def __add__(self, outro: Self) -> Self:
-        if not isinstance(outro, type(self)):
-            raise TypeError(f'Só pode somar {type(self)!r} com outras instâncias do mesmo tipo, não com {type(outro)!r}')
+    @property
+    def turmas(self) -> list[Turma]: return self._turmas
+
+
+    def __add__(self, outra_lista: Self) -> Self:
+        if not isinstance(outra_lista, type(self)):
+            raise TypeError(f'Só pode somar {type(self)!r} com outras instâncias do mesmo tipo, não com {type(outra_lista)!r}')
 
         soup_copia = copy.copy(self._soup)
-        outro_disciplinas = outro._soup.tbody.find_all('tr')
-        soup_copia.tbody.extend(outro_disciplinas)
+        outro_soup = outra_lista._soup.tbody.find_all('tr')
+        soup_copia.tbody.extend(outro_soup)
+
         return self.__class__(soup_copia)
 
 
-    def __iadd__(self, outro: Self) -> Self:
-        if not isinstance(outro, type(self)):
-            raise TypeError(f'Só pode somar {type(self)!r} com outras instâncias do mesmo tipo, não com {type(outro)!r}')
+    def __iadd__(self, outra_lista: Self) -> Self:
+        if not isinstance(outra_lista, type(self)):
+            raise TypeError(f'Só pode somar {type(self)!r} com outras instâncias do mesmo tipo, não com {type(outra_lista)!r}')
 
-        # Precisa ser deepcopy para não remover as disciplinas do código fonte em outro._soup
-        outro_disciplinas = [copy.deepcopy(disc) for disc in outro.disciplinas]
-        self._soup.tbody.extend([disc._soup for disc in outro_disciplinas])
-        self.disciplinas.extend(outro_disciplinas)
+        # Precisa ser deepcopy para não remover as turmas do código fonte em outra_lista._soup
+        outras_turmas = [copy.deepcopy(tur) for tur in outra_lista.turmas]
+        self._soup.tbody.extend([tur._soup for tur in outras_turmas])
+        self._turmas.extend(outras_turmas)
+
         return self
 
 
@@ -171,15 +194,15 @@ class ListaDisciplinas:
         if self._soup is None:
             return []
 
-        return [disc.nome for disc in self.disciplinas]
+        return [tur.nome_disciplina for tur in self._turmas]
 
 
-    def selecionar_horarios(self, dia_horario: dict[horario.DiaDaSemana, list[horario.Horario|str]]) -> list[Disciplina]:
-        disciplinas: list[Disciplina] = []
-        for disc in self.disciplinas:
+    def selecionar_horarios(self, dia_horario: dict[horario.DiaDaSemana, list[horario.Horario|str]]) -> list[Turma]:
+        turmas: list[Turma] = []
+        for tur in self._turmas:
             turma_com_horario_valido = True
 
-            for dia_turma, horarios_turma in disc.horario.items():
+            for dia_turma, horarios_turma in tur.horario.items():
                 if dia_turma not in dia_horario or not turma_com_horario_valido:
                     # Avança para a próxima disciplina
                     turma_com_horario_valido = False
@@ -196,6 +219,6 @@ class ListaDisciplinas:
                         break
 
             if turma_com_horario_valido:
-                disciplinas.append(disc)
+                turmas.append(tur)
 
-        return disciplinas
+        return turmas
