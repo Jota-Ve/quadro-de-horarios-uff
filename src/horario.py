6695f58b321715e14e7cc8da723ec905fab78b30
João Vitor Barbosa <71910512+Jota-Ve@users.noreply.github.com>
2026-02-17 08:36:34 -0300

    Adiciona testes iniciais para classe Horario

--- src/horario.py
+++ src/horario.py
@@ -1,6 +1,7 @@
-from collections import abc
 import enum
 import re
+from collections import abc
+from typing import Any
 
 
 class DiaDaSemana(enum.StrEnum):
@@ -13,40 +14,45 @@ class DiaDaSemana(enum.StrEnum):
 
 
 
-class Horario(abc.Container):
+class Horario(abc.Container[str]):
     __slots__ = ('__horario', '__inicio', '__fim')
-    
-    RGXP_HORARIO = re.compile(r'^\d\d:\d\d-\d\d:\d\d')
+
+    # Formato HH:MM-HH:MM, 24h
+    RGXP_HHMM_HHMM = re.compile(r'^(([01]\d|2[0-3]):([0-5]\d))-(([01]\d|2[0-3]):([0-5]\d))$')
 
     @property
     def inicio(self): return self.__inicio
     @property
     def fim(self): return self.__fim
-    
-    
+
+
     def __init__(self, horario: str) -> None:
         # super().__init__()
-        if not self.RGXP_HORARIO.fullmatch(horario):
-            raise ValueError(f"{horario!r} não está no padrão esperado: {self.RGXP_HORARIO.pattern!r}")
-        
+        if not self.valido(horario):
+            raise ValueError(f"{horario!r} não está no padrão esperado: {self.RGXP_HHMM_HHMM.pattern!r}")
+
         self.__horario = horario
-        self.__inicio = horario[:5]
-        self.__fim = horario[6:]
-    
-    
+        self.__inicio  = horario[:5]
+        self.__fim     = horario[6:]
+
+
     def __str__(self) -> str:
         return self.__horario
-    
+
     def __repr__(self) -> str:
-        return f'{self.__class__.__qualname__}({self.__horario})'
-    
-    
-    def __contains__(self, x) -> bool:
+        return f'{self.__class__.__qualname__}({self.__horario!r})'
+
+
+    def __contains__(self, x: Any) -> bool:
         if isinstance(x, self.__class__):
             return self.__inicio <= x.__inicio <= x.__fim <= self.__fim
-        if isinstance(x, str) and self.RGXP_HORARIO.fullmatch(x):
+        if isinstance(x, str) and self.valido(x):
             inicio, fim = x.split('-')
             return self.__inicio <= inicio <= fim <= self.__fim
-         
+
         return False
-    
+
+    @classmethod
+    def valido(cls, horario: str) -> bool:
+        return ((match := cls.RGXP_HHMM_HHMM.fullmatch(horario)) is not None
+                and match.group(1) < match.group(4))
---
+++ tests/test_horario.py
@@ -0,0 +1,27 @@
+from src.horario import Horario
+
+
+class TestHorario:
+    def test_valido_passa(self):
+        assert Horario.valido("08:00-10:00")
+
+    def test_valido_inicio_e_fim_iguais_falha(self):
+        assert not Horario.valido("08:00-08:00")
+
+    def test_valido_inicio_maior_que_fim_falha(self):
+        assert not Horario.valido("10:00-08:00")
+
+    def test_valido_formato_HMM_HHMM_falha(self):
+        assert not Horario.valido("8:00-10:00")
+
+    def test_valido_formato_HHMM_HMM_falha(self):
+        assert not Horario.valido("08:00-10:0")
+
+    def test_valido_formato_separador_incorreto_falha(self):
+        assert not Horario.valido("08:00/10:00")
+
+    def test_contem(self):
+        h = Horario("08:00-10:00")
+        assert "08:30-09:30" in h
+        assert "07:30-09:30" not in h
+        assert "08:30-10:30" not in h
