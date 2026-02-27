e9ab119371d0a5d069b8a6845eeea375d1488c47
João Vitor Barbosa <71910512+Jota-Ve@users.noreply.github.com>
2026-02-27 00:35:01 -0300

    Refatora testes da classe Horario para incluir validações com pytest e hypothesis

--- tests/test_horario.py
+++ tests/test_horario.py
@@ -1,26 +1,82 @@
-from src.horario import Horario
 import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+from src.horario import Horario
+
 
 class TestHorario:
 
-    # @pytest.p
-    def test_valido_passa(self):
-        assert Horario.valido("08:00-10:00")
+    # estratégia que gera qualquer intervalo HH:MM-HH:MM
+    horarios = st.tuples(
+        st.integers(0, 23), st.integers(0, 59),
+        st.integers(0, 23), st.integers(0, 59),
+    ).map(lambda hh_mm: f"{hh_mm[0]:02d}:{hh_mm[1]:02d}-{hh_mm[2]:02d}:{hh_mm[3]:02d}")
 
-    def test_valido_inicio_e_fim_iguais_falha(self):
-        assert not Horario.valido("08:00-08:00")
 
-    def test_valido_inicio_maior_que_fim_falha(self):
-        assert not Horario.valido("10:00-08:00")
+    @pytest.mark.parametrize("horario", [
+        "08:00-10:00",
+        "00:00-23:59",
+        "23:58-23:59",
+        "12:30-13:45",
+        "17:59-18:00",
+    ])
+    def test_validar_horario_valido_deve_passar(self, horario: str):
+        assert Horario.valido(horario)
 
-    def test_valido_formato_HMM_HHMM_falha(self):
-        assert not Horario.valido("8:00-10:00")
+    @given(horarios)
+    def test_validar_horario_com_inicio_antes_do_fim_deve_passar(self, horario: str):
+        inicio, fim = horario.split("-")
+        if inicio < fim:
+            assert Horario.valido(horario)
 
-    def test_valido_formato_HHMM_HMM_falha(self):
-        assert not Horario.valido("08:00-10:0")
+    @pytest.mark.parametrize("horario", [
+        "08:00-08:00",
+        "09:00-09:00",
+        "13:49-13:49",
+        "00:00-00:00",
+        "22:00-22:00",
+    ])
+    def test_validar_horario_com_inicio_e_fim_iguais_deve_falhar(self, horario: str):
+        assert not Horario.valido(horario)
 
-    def test_valido_formato_separador_incorreto_falha(self):
-        assert not Horario.valido("08:00/10:00")
+    @given(horarios)
+    def test_validar_horario_com_inicio_depois_do_fim_deve_falhar(self, horario: str):
+        inicio, fim = horario.split("-")
+        if inicio > fim:
+            assert not Horario.valido(horario)
+
+    @pytest.mark.parametrize("horario", [
+        "8:00-10:00",
+        "08:00-10:0",
+        "08:00/10:00",
+        "08:00_10:00",
+        "08001000",
+        "           ",
+        " ",
+        "",
+        "\n",
+        "08:00-10:00 ",
+        " 08:00-10:00",
+        " 08:00-10:00 ",
+    ])
+    def test_validar_formato_diferente_de_HHMM_HHMM_deve_falhar(self, horario: str):
+        assert not Horario.valido(horario)
+
+
+    @given(horarios)
+    def test_instanciar_horario_valido_deve_passar(self, horario: str):
+        if Horario.valido(horario):
+            # propriedade observável: instanciar não deve lançar erro
+            h = Horario(horario)
+            assert isinstance(h, Horario)
+
+    @given(horarios)
+    def test_instanciar_horario_invalido_deve_falhar(self, horario: str):
+        if not Horario.valido(horario):
+            # propriedade observável: se inválido, instanciar falha
+            assert pytest.raises(ValueError, lambda: Horario(horario))
+
 
     def test_contem(self):
         h = Horario("08:00-10:00")
